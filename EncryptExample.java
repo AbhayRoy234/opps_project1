17979638a58d440090fcb9e202564f2e3464e9f3
AbhayRoy234 <63885371+AbhayRoy234@users.noreply.github.com>
2020-05-22 16:30:42 +0530

    Update EncryptExample.java

--- EncryptExample.java
+++ EncryptExample.java
@@ -1,8 +1,5 @@
 package login;
 import java.security.MessageDigest;
-//import org.springframework.security.crypto.encrypt.Encryptors;
-//import org.springframework.security.crypto.encrypt.TextEncryptor;
-//import org.springframework.security.crypto.keygen.KeyGenerators;
 public class EncryptExample {
     public static void main(String[] args) {
     	EncryptExample myobj=new EncryptExample();
@@ -43,18 +40,4 @@ public class EncryptExample {
     
     
     
-//    public static final String md5(final String toEncrypt) {
-//        try {
-//            final MessageDigest digest = MessageDigest.getInstance("md5");
-//            digest.update(toEncrypt.getBytes());
-//            final byte[] bytes = digest.digest();
-//            final StringBuilder sb = new StringBuilder();
-//            for (int i = 0; i < bytes.length; i++) {
-//                sb.append(String.format("%02X", bytes[i]));
-//            }
-//            return sb.toString().toLowerCase();
-//        } catch (Exception exc) {
-//            return ""; // Impossibru!
-//        }
-//    }
 }
